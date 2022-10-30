033d4d76a2c2188af046be3eb9059901cbd7df74
th1agosalles <89206576+th1agosalles@users.noreply.github.com>
2022-10-30 13:50:07 -0300

    Update 190922_impressaoQuadrado.c

--- 190922_impressaoQuadrado.c
+++ 190922_impressaoQuadrado.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int imprime (char var, int x) {
     for (int l=1; l<=x; l++){
-        for (int c=1; c<=x; c++){
+        for (int c=1; c<=l; c++){
             printf ("%c", var);
         }
         printf ("\n");
@@ -18,3 +18,4 @@ int main() {
     printf ("Foram impressos %d caracteres no primeiro e %d no segundo", qtde, imprime ('!',5) );
     return 0;
 }
+
