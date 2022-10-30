626ba0fdaf6059a2996e89138186269d96a4dca1
th1agosalles <89206576+th1agosalles@users.noreply.github.com>
2022-10-30 13:36:52 -0300

    Create char_entreAeZ.c

---
+++ lista 04 - modularização/char_entreAeZ.c
@@ -0,0 +1,21 @@
+/*Crie uma função que determine se dado caracter está entre ‘a’ e ‘z’. Dica: Use a tabela ASCII.*/  
+
+#include <stdio.h>
+
+int intervalo(char n){
+    int retorno=0;
+    if(n>='a' && n<='z'){
+        retorno=1;
+    }
+    return retorno;
+}
+
+int main()
+{
+    char n; int resultado;
+    scanf ("%c",&n);
+    resultado=intervalo(n);
+    printf("%d",resultado);
+
+    return 0;
+}
