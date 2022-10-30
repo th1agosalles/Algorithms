e4c6a64074778a3d37474074395ff52bdf1df4fb
th1agosalles <89206576+th1agosalles@users.noreply.github.com>
2022-10-30 11:55:01 -0300

    Add files via upload

---
+++ lista 1 - revisao/ex2_eleitores.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese");
+    int total, branco, nulo, valido;
+    float percentualb, percentualn,percentualv;
+    
+    printf("Digite o número total de eleitores: ");
+    scanf("%d",&total);
+    printf("\nDigite o número de votos brancos: ");
+    scanf("%d",&branco);
+    printf("\nDigite o número de votos nulos: ");
+    scanf("%d",&nulo);
+    printf("\nDigite o número de votos validos: ");
+    scanf("%d",&valido);
+    
+    printf("\n\nbranco %d\n",branco);
+    printf("nulo %d\n",nulo);
+    printf("valido %d\n",valido);
+    
+    percentualb = (branco/total)*100;
+    percentualn = (nulo/total)*100;
+    percentualv = (valido/total)*100;
+    
+    printf("\n\nPercentual de votos brancos: %.2f%%",percentualb);
+    printf("\nPercentual de votos nulos: %.2f%%",percentualn);
+    printf("\nPercentual de votos válidos: %.2f%%",percentualv);
+    
+    
+    
+    
+    return 0;
+}
