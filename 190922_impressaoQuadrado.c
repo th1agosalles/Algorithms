619007ed6f5fdb19b844ad037d2e881b591dd9a4
th1agosalles <89206576+th1agosalles@users.noreply.github.com>
2022-10-30 13:47:45 -0300

    Create 190922_impressaoQuadrado.c

---
+++ 190922_impressaoQuadrado.c
@@ -0,0 +1,20 @@
+/*Aula 19/09/22 - Impressao de Quadrado de Caracteres*/
+
+#include <stdio.h>
+int imprime (char var, int x) {
+    for (int l=1; l<=x; l++){
+        for (int c=1; c<=x; c++){
+            printf ("%c", var);
+        }
+        printf ("\n");
+    }
+    return x * x;
+}
+
+int main() {
+    int qtde;
+    qtde = imprime ('/',4);
+    
+    printf ("Foram impressos %d caracteres no primeiro e %d no segundo", qtde, imprime ('!',5) );
+    return 0;
+}
