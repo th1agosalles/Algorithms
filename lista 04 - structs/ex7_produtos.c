be4614e844e27d86d4fa97a525a9b95b258a03c0
th1agosalles <89206576+th1agosalles@users.noreply.github.com>
2022-10-30 14:15:26 -0300

    Create ex7_produtos.c

---
+++ lista 04 - structs/ex7_produtos.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct {
+       long codigo;
+       char nome[100];
+       float preco;
+} Produto;
+
+int main()
+{
+    Produto p;
+    
+    // Entrada
+    printf("Digite codigo, nome e preco");
+    scanf ("%ld %s %f", &p.codigo, p.nome, &p.preco);
+    
+    // Saídas
+    printf("Valores Digitados\n");
+    printf ("%ld %s %.2f", p.codigo, p.nome, p.preco);
+
+    if (p.preco < 4) printf ("Produto em Promocao");
+    else             printf ("Produto Cadastrado");  
+
+    return 0;
+}
