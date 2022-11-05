da52e5426e09fc8e1cea588da643ee2406011856
Thiago Salles <89206576+th1agosalles@users.noreply.github.com>
2022-11-05 19:16:46 -0300

    Update funcao_leIdade.c

--- funcao_leIdade.c
+++ funcao_leIdade.c
@@ -1,10 +1,6 @@
 /******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
+Thiago Sales de Oliveira
+Cria uma função que lê duas idades e calcula a diferença entre as duas.
 *******************************************************************************/
 #include <stdio.h>
 #include<locale.h>
