0088a4c1796af76cba5ba694500319282ef06e33
Thiago Salles <89206576+th1agosalles@users.noreply.github.com>
2022-11-06 14:00:54 -0300

    Update diagonal.c

--- vetores e matrizes/diagonal.c
+++ vetores e matrizes/diagonal.c
@@ -1,4 +1,5 @@
-/*Declare uma matriz 5 x 5. Preencha com 1 a diagonal principal e com 0 os demais
+/*  Thiago Sales de Oliveira
+Declare uma matriz 5 x 5. Preencha com 1 a diagonal principal e com 0 os demais
 elementos. Escreva ao final a matriz obtida.*/
 
 #include<stdio.h>
