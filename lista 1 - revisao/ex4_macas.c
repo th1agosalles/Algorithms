9cd0f80a2ee352874663c01e9bcc674f01965e61
Thiago Salles <89206576+th1agosalles@users.noreply.github.com>
2022-11-06 14:17:12 -0300

    Update ex4_macas.c

--- lista 1 - revisao/ex4_macas.c
+++ lista 1 - revisao/ex4_macas.c
@@ -1,5 +1,5 @@
 /*		Thiago Sales de Oliveira
-As maçãs custam R $1,30 cada se forem compradas menos de uma dúzia, e R $1,00 se forem compradas pelo
+As maçãs custam R$1,30 cada se forem compradas menos de uma dúzia, e R$1,00 se forem compradas pelo
 menos 12. Escreva um programa que leia o número de maçãs compradas e escreva o custo total da compra.*/
 
 #include <stdio.h>
