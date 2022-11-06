ab7186d5e69ec17262ac9cbfbfc5bc21e9c665ab
Thiago Salles <89206576+th1agosalles@users.noreply.github.com>
2022-11-06 14:13:54 -0300

    Update ex2_eleitores.c

--- lista 1 - revisao/ex2_eleitores.c
+++ lista 1 - revisao/ex2_eleitores.c
@@ -1,4 +1,8 @@
 
+/*      Thiago Sales de Oliveira
+Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos brancos, nulos e
+válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores.*/
+
 #include <stdio.h>
 #include <locale.h>
 
