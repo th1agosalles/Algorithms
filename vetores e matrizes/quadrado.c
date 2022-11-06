23c45d62f8114a61ae1c9618a08df23c60acba5c
Thiago Salles <89206576+th1agosalles@users.noreply.github.com>
2022-11-06 14:05:13 -0300

    Update quadrado.c

--- vetores e matrizes/quadrado.c
+++ vetores e matrizes/quadrado.c
@@ -1,4 +1,5 @@
-/*Ler um conjunto de números reais, armazenando-o em vetor e calcular o quadrado
+/*  Thiago Sales de Oliveira
+Ler um conjunto de números reais, armazenando-o em vetor e calcular o quadrado
 das componentes deste vetor, armazenando o resultado em outro vetor. Os
 conjuntos têm 10 elementos cada. Imprimir todos os conjuntos (vetores).*/
 
